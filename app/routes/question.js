10d9bc88c75c54d71e1639656f38ab54820dbd7e
Jessica Fix <jcfix.pdx@gmail.com>
2016-04-01 13:10:37 -0700

    Add new answers and display in question template

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,5 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  return this.store.findRecord('question', params.question_id)
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question');
+    },
+    update(answer, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          answer.set(key, params[key]);
+        }
+      });
+      answer.save();
+      this.transitionTo('question');
+    }
+  }
 });
