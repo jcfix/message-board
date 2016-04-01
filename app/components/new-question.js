01ccdd4cc521df517bbadeb862477f4dfebc0c8e
Jessica Fix <jcfix.pdx@gmail.com>
2016-04-01 11:09:10 -0700

    Add new-question component; posting questions to discussion board works

---
+++ app/components/new-question.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+
+export default Ember.Component.extend({
+ addNewQuestion: false,
+ actions: {
+    questionFormShow() {
+    this.set('addNewQuestion', true);
+    },
+
+    save1() {
+      var params = {
+         book: this.get('book'),
+         question: this.get('question'),
+         author: this.get('author'),
+         message: this.get('message')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save2', params);
+    }
+  }
+});
