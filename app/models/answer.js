54b44614afef7211223b90b9f4944274d175a524
Jessica Fix <jcfix.pdx@gmail.com>
2016-04-09 16:50:37 -0700

    Update answer model and add to questions.json

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   response: DS.attr(),
-  timePosted: DS.attr(),
+  date: DS.attr(),
   question: DS.belongsTo('question', {async: true})
 });
