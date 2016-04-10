7e6fa7eb291f92c46175a98ff6fc2a92d1a9ca17
Jessica Fix <jcfix.pdx@gmail.com>
2016-04-10 10:12:50 -0700

    Update delete question so it is also removed from favorites list

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -9,6 +9,7 @@ export default Ember.Component.extend({
     delete(question) {
       if (confirm("Are you sure you want to delete this question?")) {
         this.sendAction('destroyQuestion', question);
+        this.get('favoritesList').remove(question);
       }
     }
   }
