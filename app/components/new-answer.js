698b241a8c45820c49d486a3738adaace7dc3b38
Jessica Fix <jcfix.pdx@gmail.com>
2016-04-13 19:37:51 -0700

    Fix issue saving answer to firebase by adding momentDate functionality to save date

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -7,10 +7,14 @@ export default Ember.Component.extend({
       this.set('addNewAnswer', true);
     },
     save1() {
+      var date = new Date();
+      date = date.toString().substring(4, 15);
+      var momentDate = moment(date).format('MM/DD/YYYY');
       var params = {
         author: this.get('author') ? this.get('author'): "",
         response: this.get('response') ? this.get('response'): "",
-        question: this.get('question')
+        question: this.get('question'),
+        date: momentDate,
       };
       this.set('addNewAnswer', false);
       this.sendAction('save2', params);
